6a4f0f17aa751b1b4323854a465545eef0ef7f47
Ben Overmyer <ben.overmyer@protolabs.com>
2019-06-05 08:12:42 -0500

    Add JSON field names for simplified religion

--- pkg/religion/display.go
+++ pkg/religion/display.go
@@ -6,19 +6,19 @@ import (
 
 // SimplifiedReligion is a simplified version of Religion for display
 type SimplifiedReligion struct {
-	Type string
-	LeaderTitle string
-	GatheringPlace string
-	Pantheon pantheon.SimplifiedPantheon
+	Type           string                      `json:"type"`
+	LeaderTitle    string                      `json:"leader_title"`
+	GatheringPlace string                      `json:"gathering_place"`
+	Pantheon       pantheon.SimplifiedPantheon `json:"pantheon"`
 }
 
 // Simplify returns a simplified version of a Religion
 func (religion Religion) Simplify() SimplifiedReligion {
 	simplified := SimplifiedReligion{
-		Type: religion.Class.Name,
-		LeaderTitle: religion.Class.LeaderTitle,
+		Type:           religion.Class.Name,
+		LeaderTitle:    religion.Class.LeaderTitle,
 		GatheringPlace: religion.GatheringPlace,
-		Pantheon: religion.Pantheon.Simplify(),
+		Pantheon:       religion.Pantheon.Simplify(),
 	}
 	return simplified
 }
