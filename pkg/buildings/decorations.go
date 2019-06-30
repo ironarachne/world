981921950141c9986831e8e711b27bc3c61cb76b
Ben Overmyer <ben@overmyer.net>
2019-06-30 11:08:28 -0500

    Add more variety to building styles

--- pkg/buildings/decorations.go
+++ pkg/buildings/decorations.go
@@ -4,12 +4,23 @@ import (
 	"github.com/ironarachne/world/pkg/random"
 )
 
-func getRandomDecorations() string {
-	decorations := []string{
+func getAllDecorations() []string {
+	return []string{
 		"statues stand outside doorways",
 		"signs bearing the occupants' names stand out front",
 		"geometric shapes are carved into the trim",
+		"ceilings are often painted in ornate styles",
+		"ceiling are often painted in geometric styles",
+		"decorative pillars are placed inside and out, with wealthier homes having more",
+		"wealthier houses and communal buildings often have small pools just outside",
+		"people often plant bushes around their homes",
+		"people often plant small trees around their homes",
+		"people often plant large trees around their homes",
 	}
+}
+
+func getRandomDecorations() string {
+	decorations := getAllDecorations()
 
 	return random.String(decorations)
 }
