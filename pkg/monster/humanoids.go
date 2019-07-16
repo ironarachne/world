ba05f9a7df07edb2b6678c11396d80f667215099
Ben Overmyer <ben.overmyer@protolabs.com>
2019-07-16 14:09:00 -0500

    Add initial monster package

---
+++ pkg/monster/humanoids.go
@@ -0,0 +1,78 @@
+package monster
+
+func getAllHumanoids() []Monster {
+	return []Monster{
+		Monster{
+			Name:                 "bugbear",
+			PluralName:           "bugbears",
+			SizeCategory:         "medium",
+			Type:                 "humanoid",
+			IdealTemperature:     5,
+			TemperatureTolerance: 5,
+		},
+		Monster{
+			Name:                 "bullywug",
+			PluralName:           "bullywugs",
+			SizeCategory:         "medium",
+			Type:                 "humanoid",
+			IdealTemperature:     5,
+			TemperatureTolerance: 5,
+		},
+		Monster{
+			Name:                 "goblin",
+			PluralName:           "goblins",
+			SizeCategory:         "small",
+			Type:                 "humanoid",
+			IdealTemperature:     5,
+			TemperatureTolerance: 5,
+		},
+		Monster{
+			Name:                 "hobgoblin",
+			PluralName:           "hobgoblins",
+			SizeCategory:         "medium",
+			Type:                 "humanoid",
+			IdealTemperature:     5,
+			TemperatureTolerance: 5,
+		},
+		Monster{
+			Name:                 "kobold",
+			PluralName:           "kobolds",
+			SizeCategory:         "small",
+			Type:                 "humanoid",
+			IdealTemperature:     5,
+			TemperatureTolerance: 5,
+		},
+		Monster{
+			Name:                 "ogre",
+			PluralName:           "ogres",
+			SizeCategory:         "medium",
+			Type:                 "humanoid",
+			IdealTemperature:     5,
+			TemperatureTolerance: 5,
+		},
+		Monster{
+			Name:                 "orc",
+			PluralName:           "orcs",
+			SizeCategory:         "medium",
+			Type:                 "humanoid",
+			IdealTemperature:     5,
+			TemperatureTolerance: 5,
+		},
+		Monster{
+			Name:                 "troglodyte",
+			PluralName:           "troglodytes",
+			SizeCategory:         "medium",
+			Type:                 "humanoid",
+			IdealTemperature:     5,
+			TemperatureTolerance: 5,
+		},
+		Monster{
+			Name:                 "troll",
+			PluralName:           "trolls",
+			SizeCategory:         "medium",
+			Type:                 "humanoid",
+			IdealTemperature:     5,
+			TemperatureTolerance: 5,
+		},
+	}
+}
---
+++ pkg/monster/monster.go
@@ -0,0 +1,33 @@
+package monster
+
+import "math/rand"
+
+// Monster is a monstrous creature or other intelligent threat
+type Monster struct {
+	Name                 string
+	PluralName           string
+	Description          string
+	SizeCategory         string
+	Type                 string
+	IdealTemperature     int
+	TemperatureTolerance int
+}
+
+func getAllMonsters() []Monster {
+	monsters := []Monster{}
+
+	humanoids := getAllHumanoids()
+
+	monsters = append(monsters, humanoids...)
+
+	return monsters
+}
+
+// Random returns a random monster
+func Random() Monster {
+	monsters := getAllMonsters()
+
+	monster := monsters[rand.Intn(len(monsters))]
+
+	return monster
+}
