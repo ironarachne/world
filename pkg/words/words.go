05372af00b1d7a53ae3199edfcfa707eb5598637
Ben Overmyer <ben.overmyer@protolabs.com>
2020-01-22 13:17:42 -0600

    Add NumberToWord function and tests

--- pkg/words/words.go
+++ pkg/words/words.go
@@ -4,6 +4,7 @@ Package words provides convenience methods for dealing with words and phrases
 package words
 
 import (
+  "fmt"
 	"strings"
 
 	"github.com/ironarachne/world/pkg/slices"
@@ -35,6 +36,69 @@ func CombinePhrases(phrases []string) string {
 	}
 
 	return combined
+}
+
+// NumberToWord returns the word version of a number
+func NumberToWord(number int) (string, error) {
+  var err error
+	result := ""
+
+	if number < 0 {
+		err = fmt.Errorf("can't turn negative number into word")
+		return "", err
+	}
+
+	if number > 20 {
+		err = fmt.Errorf("no support for numbers greater than 20")
+		return "", err
+	}
+
+	switch number {
+	case 0:
+		result = "zero"
+	case 1:
+		result = "one"
+	case 2:
+		result = "two"
+	case 3:
+		result = "three"
+	case 4:
+		result = "four"
+	case 5:
+		result = "five"
+	case 6:
+		result = "six"
+	case 7:
+		result = "seven"
+	case 8:
+		result = "eight"
+	case 9:
+		result = "nine"
+	case 10:
+		result = "ten"
+	case 11:
+		result = "eleven"
+	case 12:
+		result = "twelve"
+	case 13:
+		result = "thirteen"
+	case 14:
+		result = "fourteen"
+	case 15:
+		result = "fifteen"
+	case 16:
+		result = "sixteen"
+	case 17:
+		result = "seventeen"
+	case 18:
+		result = "eighteen"
+	case 19:
+		result = "nineteen"
+	case 20:
+		result = "twenty"
+	}
+
+	return result, nil
 }
 
 // Pronoun returns the right singular pronoun based on starting letter of a word
