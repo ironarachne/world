76cf2b2948444a2475f19f3639286b61942074e4
Ben Overmyer <ben@overmyer.net>
2020-03-19 13:03:39 -0500

    Split animal data into multiple files. Add flightless birds. Add snakes.

--- pkg/animal/animal.go
+++ pkg/animal/animal.go
@@ -13,10 +13,33 @@ import (
 
 // All returns all predefined animals
 func All() ([]species.Species, error) {
-	animals, err := species.Load("animals")
-	if err != nil {
-		err = fmt.Errorf("failed to load animals: %w", err)
-		return []species.Species{}, err
+	var animals []species.Species
+	var err error
+	var loaded []species.Species
+
+	fileNames := []string{
+		"bigcats",
+		"canids",
+		"cervids",
+		"equines",
+		"flightlessbirds",
+		"gamebirds",
+		"miscellaneous",
+		"pigs",
+		"primates",
+		"raptors",
+		"reptiles",
+		"rodents",
+		"waterfowl",
+	}
+
+	for _, f := range fileNames {
+		loaded, err = species.Load("animals/" + f)
+		if err != nil {
+			err = fmt.Errorf("failed to load animals: %w", err)
+			return []species.Species{}, err
+		}
+		animals = append(animals, loaded...)
 	}
 
 	return animals, nil
